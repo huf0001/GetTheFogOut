2999ae75cb0c1ecd751ec872155e644e5d5a77e1
unknown <101633177@AS409-020.ds.swin.edu.au>
2019-04-01 13:50:46 +1100

    Temp commit, ignore

---
+++ Assets/Scripts/Fog.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fog : MonoBehaviour
+{
+    private List<FogUnit> units = new List<FogUnit>();
+    private WorldController world;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        world = GameObject.Find("GameManager").GetComponent<WorldController>();
+
+
+        
+    }
+
+    private void CreateNewUnit(WorldController world, int x, int y)
+    {
+        FogUnit unit = new FogUnit();
+        unit.Location = world.GetTileAt(x, y).GetComponent<Tile>();
+
+        units.Add(unit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (units.Count == 0)
+        {
+            // game over, the player has won.
+        }
+    }
+
+
+}
