b7391bbd4ebc223b8988bb521867dac154f2c3cc
Matthew Germon <7617844@student.swin.edu.au>
2019-04-07 15:04:42 +1000

    Canvas Testing for Event triggers

--- Assets/Scripts/CanvasToolTip.cs
+++ Assets/Scripts/CanvasToolTip.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class CanvasToolTip : MonoBehaviour
 {
@@ -9,13 +10,35 @@ public class CanvasToolTip : MonoBehaviour
 	public GameObject[] Buttons;
 	
 	private Text ToolTipText;
+	private bool IsVisible;
     void Start()
     {
-        
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerEnter;
+        entry.callback.AddListener((data) => { OnPointerEnter((PointerEventData)data); });
+        trigger.triggers.Add(entry);
+
+    	ToolTip.SetActive(false);
+        ToolTipText.text = "";
+        IsVisible = false;
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+    	ToolTip.SetActive(true);
+        Debug.Log("OnPointerEnter called.");
     }
 
     void Update()
     {
-        
+        if (IsVisible)
+        {
+        	ToolTip.SetActive(true);
+        }
+        else
+        {
+        	ToolTip.SetActive(false);
+        }
     }
 }
