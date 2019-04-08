be5ca4e22b2f764eec1e6042634fef2a8100ec61
Matthew Germon <7617844@student.swin.edu.au>
2019-04-08 11:56:00 +1000

    UI Working, desplays unique text when mouse hovers over buttons

--- Assets/Scripts/CanvasToolTip.cs
+++ Assets/Scripts/CanvasToolTip.cs
@@ -7,56 +7,56 @@ using UnityEngine.EventSystems;
 public class CanvasToolTip : MonoBehaviour
 {
 	public GameObject ToolTip;
-	public GameObject[] Buttons;
-	
-	private Text ToolTipText;
-	private bool IsVisible;
+	public Text ToolTipText;
+
     void Start()
     {
-        EventTrigger trigger = GetComponent<EventTrigger>();
-        EventTrigger.Entry entry = new EventTrigger.Entry();
-        entry.eventID = EventTriggerType.PointerEnter;
-        entry.callback.AddListener((data) => { OnPointerEnter((PointerEventData)data); });
-        trigger.triggers.Add(entry);
+        // EventTrigger trigger = GetComponent<EventTrigger>();
+        // EventTrigger.Entry entry = new EventTrigger.Entry();
+        // entry.eventID = EventTriggerType.PointerEnter;
+        // entry.callback.AddListener((data) => { OnPointerEnter((PointerEventData)data); });
+        // trigger.triggers.Add(entry);
 
-    	ToolTip.SetActive(false);
         ToolTipText.text = "";
-        IsVisible = false;
+    	ToolTip.SetActive(false);
     }
 
-    public void OnPointerEnter(PointerEventData data)
+    public void NotifyToolTip(string message)
     {
-        // Code to check the value, update string to show.
-        //
-        // Battery
-        // ToolTipText.text = "Battery\nBattery description\nXps";
-        //
-        // Defence
-        // ToolTipText.text = "Defence\nDefence description\nXps";
-        //
-        // Generator
-        // ToolTipText.text = "Generator\nGenerator description\nXps";
-        //
-        // Harvester
-        // ToolTipText.text = "Harvester\nHarvester description\nXps";
-        //
-        // Relay
-        // ToolTipText.text = "Relay\nRelay description\nXps";
-
-    	ToolTip.SetActive(true);
-        Debug.Log("OnPointerEnter called.");
-
-    }
-
-    void Update()
-    {
-        if (IsVisible)
+        if (message != null)
         {
+            if (message == "Battery")
+            {
+                ToolTipText.text = "Battery\nBattery description\nXps";
+            }
+            if (message == "Defence")
+            {
+                ToolTipText.text = "Defence\nDefence description\nXps";
+            }
+            if (message == "Generator")
+            {
+                ToolTipText.text = "Generator\nGenerator description\nXps";
+            }
+            if (message == "Harvester")
+            {
+                ToolTipText.text = "Harvester\nHarvester description\nXps";
+            }
+            if (message == "Relay")
+            {
+                ToolTipText.text = "Relay\nRelay description\nXps";
+            }
             ToolTip.SetActive(true);
         }
         else
         {
-            ToolTip.SetActive(false);
+            ToolTipText.text = "Error, unknown message, ZIPPIDEE DOO.";
+            return;
         }
+    }
+
+    public void ExitToolTip()
+    {
+        ToolTip.SetActive(false);
+        ToolTipText.text = "";
     }
 }
