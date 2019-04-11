4e9023f99bea40310922804518b1d6c5c45f03d8
Liam D <101626449@student.swin.edu.au>
2019-04-11 14:17:49 +1000

    finalise basic building selection tab and merge into prefab

---
+++ Assets/Scripts/BuildingSelector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BuildingSelector : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI buildingDesc;
+
+    public void ToggleVisibility()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+        buildingDesc.gameObject.SetActive(!buildingDesc.gameObject.activeSelf);
+    }
+}
--- Assets/Scripts/btn_tower.cs
+++ Assets/Scripts/btn_tower.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class btn_tower : MonoBehaviour
+public class btn_tower : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField]
     private GameObject obj_prefab;
@@ -16,14 +17,7 @@ public class btn_tower : MonoBehaviour
     private WorldController WC;
     public KeyCode _key;
 
-    // Matt Germon: Test Code to see if moving the mouse over a button would trigger.
-    // considering as a means of making "Tooltip" appear in the Canvas.
-    //private bool IsMouseOver;
-
-    // void Start()
-    // {
-    //     IsMouseOver = false;
-    // }
+    [SerializeField] TextMeshProUGUI buildingDesc;
 
     void Update()
     {
@@ -34,19 +28,51 @@ public class btn_tower : MonoBehaviour
         }
     }
 
-    // Matt Germon: Test Code to see if moving the mouse over a button would trigger.
-    // considering as a means of making "Tooltip" appear in the Canvas.
-    // void OnMouseOver()
-    // {
-    //     IsMouseOver = true;
-    //     Debug.Log("Mouse is over the Button.");
-    // }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        switch (gameObject.name)
+        {
+            case "btn_battery":
+                buildingDesc.text = "<b>Battery</b>\n" +
+                    "Build Cost: 30 <sprite=\"all_icons\" index=0>\n" +
+                    "Running Cost: 0\n" +
+                    "Effect: +10 max storage <sprite=\"all_icons\" index=0>";
+                break;
+            case "btn_generator":
+                buildingDesc.text = "<b>Power Generator</b>\n" +
+                    "Build Cost: 30 <sprite=\"all_icons\" index=0>\n" +
+                    "Running Cost: 0\n" +
+                    "Effect: +2 <sprite=\"all_icons\" index=0> /s";
+                break;
+            case "btn_harvester":
+                buildingDesc.text = "<b>Elemental Harvester</b>\n" +
+                    "Build Cost: 50 <sprite=\"all_icons\" index=0>\n" +
+                    "Running Cost: 5 <sprite=\"all_icons\" index=0> /s\n" +
+                    "Effect: +5 <sprite=\"all_icons\" index=0>/<sprite=\"all_icons\" index=1>/<sprite=\"all_icons\" index=2>/<sprite=\"all_icons\" index=3> /s";
+                break;
+            case "btn_defence":
+                buildingDesc.text = "<b>Cluster Fan</b>\n" +
+                    "Build Cost: 50 <sprite=\"all_icons\" index=0>\n" +
+                    "Running Cost: 5 <sprite=\"all_icons\" index=0> /s\n" +
+                    "Effect: Damages fog\n" +
+                    "Damage: 50 centre, 25 adjacent";
+                break;
+            case "btn_relay":
+                buildingDesc.text = "<b>Power Relay</b>\n" +
+                    "Build Cost: 10 <sprite=\"all_icons\" index=0>\n" +
+                    "Running Cost: 0\n" +
+                    "Effect: Extends range from Hub";
+                break;
+            default:
+                buildingDesc.text = "";
+                break;
+        }
+    }
 
-    // void OnMouseExit()
-    // {
-    //     IsMouseOver = false;
-    //     Debug.Log("Mouse is off the Button.");
-    // }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buildingDesc.text = "";
+    }
 
     private void Awake()
     {
