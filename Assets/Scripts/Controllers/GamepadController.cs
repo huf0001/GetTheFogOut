3d25c5a540019beacccd7a1296ec390d26d14bec
Matthew Germon <7617844@student.swin.edu.au>
2019-04-24 09:32:55 +1000

    Test Xbox One Controller set

--- Assets/Scripts/Controllers/GamepadController.cs
+++ Assets/Scripts/Controllers/GamepadController.cs
@@ -29,36 +29,6 @@
 */
 //////////////////////////////////////////////////////////////////////////////
 
-
-//////////////////////////////////////////////////////////////////////////////
-//
-// Sources: https://answers.unity.com/questions/1350081/xbox-one-controller-mapping-solved.html
-// Sources: https://ritchielozada.com/2016/01/08/part-9-configure-input-settings-for-camera-controls/
-// Sources: https://ritchielozada.com/2016/01/16/part-11-using-an-xbox-one-controller-with-unity-on-windows-10/
-//
-//  A                                  joystick button 0
-//  B                                  joystick button 1
-//  X                                  joystick button 2
-//  Y                                  joystick button 3
-//  Left Bumper                        joystick button 4
-//  Right Bumper                       joystick button 5
-//  View (Back)                        joystick button 6
-//  Menu (Start)                       joystick button 7
-//  Left Stick Button                  joystick button 8
-//  Right Stick Button                 joystick button 9
-//  Left Stick “Horizontal”            X Axis               -1 to 1
-//  Left Stick “Vertical”              Y Axis                1 to -1
-//  Left Trigger Shared Axis           3rd Axis              0 to 1
-//  Right Trigger Shared Axis          3rd Axis              0 to -1 
-//  Right Stick “HorizontalTurn”       4th Axis             -1 to 1
-//  Right Stick “VerticalTurn”         5th Axis              1 to -1
-//  DPAD – Horizontal                  6th Axis             -1 (.64) 1
-//  DPAD – Vertical                    7th Axis             -1 (.64) 1
-//  Left Trigger                       9th Axis              0 to 1
-//  Right Trigger                      10th Axis             0 to 1
-// 
-//////////////////////////////////////////////////////////////////////////////
-
 public class GamepadController : MonoBehaviour
 {
 
