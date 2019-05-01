fc381391504f52c5b6df26276741927d3322eacf
Matthew Germon <7617844@student.swin.edu.au>
2019-05-01 15:50:05 +1000

    Camera BuildingSelector issue, Take 3

--- Assets/Scripts/Buildings/TowerManager.cs
+++ Assets/Scripts/Buildings/TowerManager.cs
@@ -58,7 +58,7 @@ public class TowerManager : MonoBehaviour
 
     public void EscToCancel()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Xbox_B"))
         {
             selectedTower = null;
             InbuildMode = false;
