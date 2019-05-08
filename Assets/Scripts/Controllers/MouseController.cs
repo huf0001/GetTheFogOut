7980942afee8a15aa3402a2929b0586442d42f36
Dunstatron <liam_cg_dunstan@yahoo.com.au>
2019-05-08 17:44:20 +1000

    block bugzzzz, can't build other buildings on nodes and vice-versa Deleting is not going to work for harvesters for the moment

--- Assets/Scripts/Controllers/MouseController.cs
+++ Assets/Scripts/Controllers/MouseController.cs
@@ -206,7 +206,7 @@ public class MouseController : MonoBehaviour
                     {
                         if (!UIController.instance.buildingSelector.Visible)
                         {
-                            UIController.instance.buildingSelector.ToggleVisibility();
+                            UIController.instance.buildingSelector.ToggleVisibility(tile);
                         }
 
                         if (reportTutorialClick)
