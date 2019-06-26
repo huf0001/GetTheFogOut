ba80ab61597449d36be5bb27fccd204c400bc9a5
Dunstatron <liam_cg_dunstan@yahoo.com.au>
2019-06-26 16:35:26 +1000

    add difficulty selection and most functionality dropdown is extremely buggy for some reason, also branch needs to be merged with master to finalise the difficulty selection

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,35 +5,98 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private string gameScene;
-    [SerializeField] private Image playButton;
-    [SerializeField] private TextMeshProUGUI playText;
-    [SerializeField] private Image quitButton;
-    [SerializeField] private TextMeshProUGUI quitText;
-    [SerializeField] private TextMeshProUGUI titleText;
-    [SerializeField] private CanvasGroup tutorialToggle;
+    [SerializeField] private CanvasGroup mainCanvasGroup;
+    [SerializeField] private TextMeshProUGUI difficultyButtonText;
     [SerializeField] private Slider loadingBar;
     [SerializeField] private CanvasGroup loadingCanvasGroup;
+    [SerializeField] private CanvasGroup difficultySubmenu;
+    [SerializeField, TextArea] private string[] difficultyDescriptions;
+    [SerializeField] private TextMeshProUGUI difficultyDescText;
+
     private bool skipTutorial = false;
+    private int difficulty = 1;
 
     public void ToggleTutorial(bool tutorialOn)
     {
         skipTutorial = !tutorialOn;
     }
 
+    public void ChangeDifficulty(int diff)
+    {
+        difficulty = diff;
+        difficultyDescText.text = difficultyDescriptions[difficulty];
+        StartCoroutine(Unhover());
+    }
+
+    private IEnumerator Unhover()
+    {
+        difficultySubmenu.blocksRaycasts = false;
+        yield return new WaitForSeconds(0.1f);
+        difficultySubmenu.blocksRaycasts = true;
+    }
+
+    public void OpenMenu()
+    {
+        StartCoroutine(OpenDifficultyMenu());
+    }
+
+    private IEnumerator OpenDifficultyMenu()
+    {
+        mainCanvasGroup.blocksRaycasts = false;
+        yield return new WaitForSeconds(0.1f);
+        mainCanvasGroup.interactable = false;
+        mainCanvasGroup.DOFade(0, 0.5f)
+            .OnComplete(
+            delegate
+            {
+                difficultySubmenu.DOFade(1, 0.5f);
+                difficultySubmenu.interactable = true;
+                difficultySubmenu.blocksRaycasts = true;
+            });
+    }
+
+    public void CloseMenu()
+    {
+        StartCoroutine(CloseDifficultyMenu());
+    }
+
+    private IEnumerator CloseDifficultyMenu()
+    {
+        switch (difficulty)
+        {
+            case 0:
+                difficultyButtonText.text = "Difficulty: Easy";
+                break;
+            case 1:
+                difficultyButtonText.text = "Difficulty: Medium";
+                break;
+            case 2:
+                difficultyButtonText.text = "Difficulty: Hard";
+                break;
+        }
+        difficultySubmenu.blocksRaycasts = false;
+        yield return new WaitForSeconds(0.1f);
+        difficultySubmenu.interactable = false;
+        difficultySubmenu.DOFade(0, 0.5f)
+            .OnComplete(
+            delegate
+            {
+                mainCanvasGroup.DOFade(1, 0.5f);
+                mainCanvasGroup.interactable = true;
+                mainCanvasGroup.blocksRaycasts = true;
+            });
+    }
+
     public void playGame()
     {
         GlobalVars.SkipTut = skipTutorial;
-        Sequence sequence = DOTween.Sequence();
-        sequence.Insert(0, playButton.DOFade(0, 0.5f))
-            .Insert(0, quitButton.DOFade(0, 0.5f))
-            .Insert(0, titleText.DOFade(0, 0.5f))
-            .Insert(0, quitText.DOFade(0, 0.5f))
-            .Insert(0, playText.DOFade(0, 0.5f))
-            .Insert(0, tutorialToggle.DOFade(0, 0.5f))
+        GlobalVars.Difficulty = difficulty;
+        mainCanvasGroup.DOFade(0, 0.5f)
             .OnComplete(
             delegate
             {
