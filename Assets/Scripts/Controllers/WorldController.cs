016ac821b04f7409c06e2b7ba02caecff08a7696
david101444751 <101444751@student.swin.edu.au>
2019-07-19 13:58:42 +1000

    IMPROVE
    -remove redundant loop code in worldcontroller
    -add condition check to avoid overlapped tiles

--- Assets/Scripts/Controllers/WorldController.cs
+++ Assets/Scripts/Controllers/WorldController.cs
@@ -658,13 +658,17 @@ public class WorldController : MonoBehaviour
                 {
                     if ((tile.Z == 19) || (tile.Z == 20) || (tile.Z == 21))
                     {
-                        activeTiles.Add(tile);
+                        if (!activeTiles.Contains(tile))
+                        {
+                            activeTiles.Add(tile);
+                        }
                         DisableTiles.Remove(tile);
                         tile.PowerUp(tempPower);
+           //             Debug.Log(tile.Name);
                     }
                 }
             }
-            Debug.Log("off");
+        //    Debug.Log("off");
             thrusterToggle = true;
         }
     }
@@ -687,10 +691,11 @@ public class WorldController : MonoBehaviour
                         }
                         tempPower = tile.getself().PowerSource;
                         tile.PowerDown(tempPower);
+                   //     Debug.Log(tile.Name);
                     }
                 }
             }
-            Debug.Log("on");
+        //    Debug.Log("on");
             thrusterToggle = false;
         }
     }
