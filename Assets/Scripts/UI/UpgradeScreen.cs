0033a52a3392721df69ecfd6222c0df689d68553
liamcgd <liam_cg_dunstan@yahoo.com.au>
2019-08-16 15:41:00 +1000

    complete visual functionality of upgrade screen

---
+++ Assets/Scripts/UI/UpgradeScreen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeScreen : MonoBehaviour
+{
+    [Header("Button Colours")]
+    [SerializeField] private Color upgradedButton;
+    [SerializeField] private Color upgradedLine;
+    [SerializeField] private Color unupgradedLine;
+    [SerializeField, Tooltip("Alpha value for other tree")] private float treeUnavailable;
+
+    public void PerformUpgrade(Image image, CanvasGroup otherTree)
+    {
+        
+    }
+
+    public void ChangeCurButtonColour(Image image)
+    {
+        image.color = upgradedButton;
+    }
+
+    public void ChangeOtherTreeColour(CanvasGroup canvasGroup)
+    {
+        canvasGroup.alpha = treeUnavailable;
+        canvasGroup.interactable = false;
+    }
+
+    public void ChangeLineColour(Image image)
+    {
+        image.color = upgradedLine;
+    }
+
+    public void ChangeOtherTreeLineColour(Image image)
+    {
+        image.color = unupgradedLine;
+    }
+
+    public void UnlockNextUpgrade(Button button)
+    {
+        button.interactable = true;
+    }
+}
