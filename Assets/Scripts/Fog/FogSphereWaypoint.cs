3fb8b5d9d3c830ee7656520e7a4d66906c5e47c8
huf0001 <samhuffer01@gmail.com>
2019-09-04 11:59:48 +1000

    Put together a waypoint prefab and a network of them. They all point to the next waypoint(s) that the fog spheres would go to. Just need to get the fog spheres moving according to their positions, and the fog spheres should be all good to go.

---
+++ Assets/Scripts/Fog/FogSphereWaypoint.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FogSphereWaypoint : MonoBehaviour
+{
+    [SerializeField] private List<FogSphereWaypoint> nextWaypoints;
+    [SerializeField] private bool renderLines;
+    [SerializeField] private bool spawnPoint;
+    [SerializeField] private bool hub;
+
+    public bool SpawnPoint { get => spawnPoint; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Round(pos.x);
+        pos.z = Mathf.Round(pos.z);
+        transform.position = pos;
+
+        if (!hub)
+        {
+            if (spawnPoint)
+            {
+                name = $"FogSphereSpawnPoint({pos.x},{pos.z})";
+            }
+            else
+            {
+                name = $"FogSphereWaypoint({pos.x},{pos.z})";
+            }
+        }        
+
+        if (renderLines)
+        {
+            foreach (FogSphereWaypoint w in nextWaypoints)
+            {
+                if (w != null)
+                {
+                    Debug.DrawLine(transform.position, Vector3.MoveTowards(transform.position, w.transform.position, 2f), Color.cyan);
+                }
+            }
+        }        
+    }
+
+    public FogSphereWaypoint GetNextWaypoint()
+    {
+        if (nextWaypoints.Count == 0)
+        {
+            return null;
+        }
+        else if (nextWaypoints.Count == 1)
+        {
+            return nextWaypoints[0];
+        }
+        else
+        {
+            return nextWaypoints[UnityEngine.Random.Range(0, nextWaypoints.Count)];
+        }
+    }
+}
