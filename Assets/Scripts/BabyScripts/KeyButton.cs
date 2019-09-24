f409521ebecc5ae982d566ac301c429cce0978a6
CyrusEdgren <101606991@student.swin.edu.au>
2019-09-25 09:33:04 +1000

    Added hotkeys for ability buttons. Also fixed shield health appearing when bulding damaged without shield effect.

---
+++ Assets/Scripts/BabyScripts/KeyButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+// Some code taken from https://answers.unity.com/questions/934527/ui-button-click-through-keyboardcode.html
+
+[RequireComponent(typeof(Button))]
+public class KeyButton : MonoBehaviour
+{
+    public Key key;
+    public Button button {get; private set;}
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current[key].wasPressedThisFrame && button.interactable && AbilityMenu.Instance.Visible)
+        {
+            button.onClick.Invoke();
+        }
+    }
+}
